0342d7db347eb13aa5b3182fb8c4c0e23cb2f494
Juanjo2222 <juanjomillanguerra@gmail.com>
2025-04-04 12:03:00 +0200

    refactor(pages): defaultLayout added to index.vue

--- technicalTest/pages/index.vue
+++ technicalTest/pages/index.vue
@@ -1,14 +1,12 @@
 <script>
 definePageMeta({
   defaultLayout: "default-layout",
-  channelLayout: "channel-layout",
 });
 </script>
 
 <template>
   <div class="container">
     <NuxtLayout :name="$route.meta.defaultLayout"/>
-    <NuxtLayout :name="$route.meta.channelLayout"/>
   </div>
 </template>
 
