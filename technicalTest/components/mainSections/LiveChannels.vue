437859d6a42fe020ea6af44816f2240657fef316
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-05 19:55:16 +0200

    feat(components): live channel rows

--- technicalTest/components/mainSections/LiveChannels.vue
+++ technicalTest/components/mainSections/LiveChannels.vue
@@ -1,81 +1,68 @@
 <script setup lang="ts">
+import IconDownArrow from '@/components/icons/DownArrowIcon.vue';
+import StreamerCard from '@/components//mainSections/liveChannels/StreamerCard.vue';
+
 const channels = [
-  { id: 1, name: "Streamer1", category: "category", thumbnail: "https://placehold.co/150", language: "español" },
-  { id: 2, name: "Streamer2", category: "category", thumbnail: "https://placehold.co/150", language: "español" },
-  { id: 3, name: "Streamer3", category: "category", thumbnail: "https://placehold.co/150", language: "español" }
+  { id: 1, name: "Streamer1", title:"title", category: "category", thumbnail: "https://placehold.co/150", language: "español", drops: "drops" },
+  { id: 2, name: "Streamer2", title:"title", category: "category", thumbnail: "https://placehold.co/150", language: "español", drops: "drops" },
+  { id: 3, name: "Streamer3", title:"title", category: "category", thumbnail: "https://placehold.co/150", language: "español", drops: "drops" }
 ];
+const blueText = "Live channels";
 </script>
 
 <template>
   <section class="live-channels-section">
-    <h2 class="title">Live Channels we think you'll like</h2>
-    <section class="live-channels">
-      <div v-for="channel in channels" :key="channel.id" class="live-channels__card">
-        <img :src="channel.thumbnail" class="live-channels__card--image">
-        <div class="live-channels__card--info">
-          <span class="live-channels__card--info-name">{{ channel.name }}</span>
-          <span class="live-channels__card--info-category">{{ channel.category }}</span>
-          <span class="live-channels__card--info-language">{{ channel.language }}</span>
-        </div>
-      </div>
-    </section>
+    <h2 class="live-channels-section__title">
+      <span class="live-channels-section__blue-text">
+        {{ blueText }}
+      </span> 
+      we think you'll like
+    </h2>
+
+    <StreamerCard :channels="channels" />
+
+    <div class="live-channels-section__divider">
+      <span class="live-channels-section__divider--text">Show more</span>
+      <IconDownArrow class="arrow-icon" />
+    </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+.live-channels-section {
+  background-color: #0e0e10;
+  margin-right: auto;
 
-.live-channels-section{
-  margin-top:5rem;
-}
+  &__title {
+    font-family: Arial, Helvetica, sans-serif;
+    color: #ffffff;
+    margin-top: auto;
+    padding-top: 1.2rem;
+  }
 
-.title {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-bottom: 1rem;
-  color: #ff0000;
-}
+  &__blue-text {
+    color: #1889df;
+  }
 
-.live-channels {
-  margin-bottom: 2rem;
-  font-family: Arial, Helvetica, sans-serif;
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-  background-color: #3a3aee;
-
-  &__card {
+  &__divider {
     display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    background-color: #b8ee3a;
-    width: 23.9rem;
+    align-items: center;
+    color: #199afc;
+    font-weight: bold;
 
-    &--image {
-      width: 23.875rem;
-      height: 13.1512rem;
-      object-fit: cover;
+    &::before,
+    &::after {
+      content: '';
+      flex: 1;
+      margin-left: 1rem;
+      border-top: 0.0625rem solid #444;
     }
 
-    &--info {
-      display: flex;
-      flex-direction: column;
-      
-      &-name {
-        font-size: 1rem;
-        font-weight: bold;
-        color: #fff;
-        margin-top: 0.5rem;
-      }
-
-      &-category {
-        font-size: 0.85rem;
-        color: #b3b3b3;
-        margin-top: 0.25rem;
-      }
-    }
-    &:hover {
-      background: #2e2e38;
+    &--text {
+      font-family: Arial, Helvetica, sans-serif;
+      padding: 0.625rem;
+      font-size: 0.875rem;
+      cursor: pointer;
     }
   }
 }
