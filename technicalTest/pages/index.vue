82dc1d0d48a69065704c69b419b3f5fc7b20d695
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-05 19:57:14 +0200

    feat(pages): liveChannels added to index.vue

--- technicalTest/pages/index.vue
+++ technicalTest/pages/index.vue
@@ -1,15 +1,30 @@
-<script>
+<script setup>
+import LiveChannels from '@/components/mainSections/LiveChannels.vue';
+
 definePageMeta({
   defaultLayout: "default-layout",
 });
 </script>
 
 <template>
-  <div class="container">
-    <NuxtLayout :name="$route.meta.defaultLayout"/>
+  <div class="main">
+    <NuxtLayout :name="$route.meta.defaultLayout">
+      <section class="main__live-channels">
+        <LiveChannels/>
+        <LiveChannels/>
+        <LiveChannels/>
+        <LiveChannels/>
+      </section>
+    </NuxtLayout>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+  main{
+    background-color: #0e0e10;
+    
+    &__live-channels{
+      display: flex;
+    }
+  }
 </style>
