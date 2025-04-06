2be6cc6454e688a15ab8c91fd44bace513c76b05
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-06 17:45:31 +0200

    feat(components): buttons for category row created

---
+++ technicalTest/components/mainSections/categories/CategoryButtons.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import GameCategIcon from '@/components/icons/GameCategIcon.vue';
+import IrlCategIcon from '@/components/icons/IrlCategIcon.vue';
+import MusicCategIcon from '@/components/icons/MusicCategIcon.vue';
+import CreativeCategIcon from '@/components/icons/CreativeCategIcon.vue';
+import SportsCategIcon from '@/components/icons/SportsCategIcon.vue';
+
+const buttons = [
+  { label: "Games", icon: GameCategIcon },
+  { label: "IRL", icon: IrlCategIcon },
+  { label: "Music & DJs", icon: MusicCategIcon },
+  { label: "Creative", icon: CreativeCategIcon },
+  { label: "Esports", icon: SportsCategIcon }
+];
+</script>
+
+<template>
+    <div class="category-button-container">
+      <button
+        v-for="(button, index) in buttons"
+        :key="index"
+        class="category-button"
+      >
+        <span class="category-button__text">{{ button.label }}</span>
+        <component :is="button.icon" class="category-button__icon" />
+      </button>
+    </div>
+  </template>
+
+<style scoped lang="scss">
+
+.category-button-container {
+  display: flex;
+  gap: 1.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+.category-button{
+  background-color: #199afc;
+  border-radius: 0.5rem;
+  border: none;
+  padding-left: 1rem;
+  width: 18rem;
+  height: 2.8rem;
+  cursor: pointer;
+  display:flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &__text{
+    color: white;
+    font-weight: bold;
+    font-size: 1.3rem;
+  }
+  
+  &__icon{
+    margin-left: auto;
+  }
+
+  &:hover{
+    opacity: 0.8;
+  }
+
+}
+
+</style>
