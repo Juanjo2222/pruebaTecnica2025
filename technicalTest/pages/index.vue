d11c38735d202d8401f8d2f489a8ab73154ffbef
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-15 19:23:53 +0200

    refactor(components): index.vue refactorized

--- technicalTest/pages/index.vue
+++ technicalTest/pages/index.vue
@@ -9,24 +9,10 @@ definePageMeta({
 
 <template>
   <div class="main">
-    <NuxtLayout name="default-layout">
+    <NuxtLayout :name="$route.meta.defaultLayout">
       <section class="main__live-channels">
-        <LiveChannels/>
-        <LiveChannels/>
-        <CategoriesRow/>
-        <LiveChannels/>
         <LiveChannels/>
       </section>
     </NuxtLayout>
   </div>
 </template>
-
-<style scoped lang="scss">
-  main{
-    background-color: #0e0e10;
-    
-    &__live-channels{
-      display: flex;
-    }
-  }
-</style>
