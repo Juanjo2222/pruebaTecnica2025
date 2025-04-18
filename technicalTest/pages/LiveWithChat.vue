b03724cd134a1f2ba45f6c8055e5ec26934c1328
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-18 16:35:39 +0200

    feat(page): streamer live with its chat page

---
+++ technicalTest/pages/LiveWithChat.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LiveComponent from '@/components/mainSections/liveWithChat/LiveComponent.vue';
+import ChatComponent from '@/components/mainSections/liveWithChat/ChatComponent.vue';
+
+</script>
+
+<template>
+  <section class="live-chat-section">
+    <LiveComponent/>
+    <ChatComponent/>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+.live-chat-section{
+  display: flex;
+}
+
+</style>
