a8eda7ee39c833639e84392dc96fd441b786ba1c
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-20 21:17:21 +0200

    feat(types): new properties added to type streamer.ts

--- technicalTest/types/streamer.ts
+++ technicalTest/types/streamer.ts
@@ -1,9 +1,14 @@
 export type Streamer = {
+  id: number;
+  user_id: string;
   user_name: string;
+  user_login: string;
+  description: string;
   title: string;
   game_name: string;
   thumbnail_url: string;
   viewer_count: number;
-  profileImg: string;
+  profile_img: string;
+  started_at: string;
   tags: string[];
 };
