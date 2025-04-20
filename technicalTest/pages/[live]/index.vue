4eacfeaa72435e95f7bb1c2e094d1363b82a7142
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-20 21:18:48 +0200

    feat(pages): dinamic page for live and chat

--- technicalTest/pages/[live]/index.vue
+++ technicalTest/pages/[live]/index.vue
@@ -2,12 +2,20 @@
 import LiveComponent from '@/components/mainSections/liveWithChat/LiveComponent.vue';
 import ChatComponent from '@/components/mainSections/liveWithChat/ChatComponent.vue';
 
+const route = useRoute();
+
+definePageMeta({
+  defaultLayout: "default-layout",
+});
+
 </script>
 
 <template>
   <section class="live-chat-section">
-    <LiveComponent/>
-    <ChatComponent/>
+    <NuxtLayout :name="$route.meta.defaultLayout">
+      <LiveComponent :user_login="route.params.live"/>
+      <ChatComponent :user_login="route.params.live"/>
+    </NuxtLayout>
   </section>
 </template>
 
