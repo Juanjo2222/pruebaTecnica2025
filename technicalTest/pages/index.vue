653d23b625eaf30193dfa0f99d2d7289f4a30585
Juanjo <juanjosemillanguerra@gmail.com>
2025-04-21 18:42:05 +0200

    refactor(pages): page title added

--- technicalTest/pages/index.vue
+++ technicalTest/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import LiveChannels from '@/components/mainSections/LiveChannels.vue';
+import texts from '@/assets/data/texts.json';
 
 definePageMeta({
   defaultLayout: "default-layout",
@@ -8,6 +9,7 @@ definePageMeta({
 
 <template>
   <div class="main">
+    <title>{{ texts.pageTitle }}</title>
     <NuxtLayout :name="$route.meta.defaultLayout">
       <section class="main__live-channels">
         <LiveChannels/>
